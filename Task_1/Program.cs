7d54c7f01ad0d996ef084aaa91b9bb3e6af7b9fb
Vitalii Khrystov <vitalik_khristov@ukr.net>
2022-09-14 18:46:16 +0300

    Task 1. Added: Class Startup, Folders Controller, Model, View.

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -4,12 +4,18 @@ namespace Task_1
     {
         public static void Main(string[] args)
         {
+            //Задание 1 (Additional Task)
+            //Создайте пустое ASP.NET Core приложение.Внесите в него нужные изменения, для
+            //использования MVC.Сделайте необходимые изменения в проекте, чтобы при запросе
+            /// Test / Message отображалась страница с сообщением «Hello world», а при запросе List / Info -
+            //отображался список<ul> с тремя элементами и произвольным текстом.
+
+
             var builder = WebApplication.CreateBuilder(args);
+            var startup = new Startup(builder.Configuration);
+            startup.ConfigureServices(builder.Services); // calling ConfigureServices method
             var app = builder.Build();
-
-            app.MapGet("/", () => "Hello World!");
-
-            app.Run();
+            startup.Configure(app, builder.Environment); // calling Configure method
         }
     }
 }
---
+++ Task_1/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Task_1
+{
+    public class Startup
+    {
+        public IConfiguration configRoot
+        {
+            get;
+        }
+        public Startup(IConfiguration configuration)
+        {
+            configRoot = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddMvc();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+           
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseMvc();
+            app.MapRazorPages();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
